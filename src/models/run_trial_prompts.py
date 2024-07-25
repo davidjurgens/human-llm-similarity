84610850e1e26a742104f08ed5a773580064339f
Lechen Zhang <leczhang@umich.edu>
2024-07-24 23:03:44 -0400

    Switch output format from json to jsonl

--- src/models/run_trial_prompts.py
+++ src/models/run_trial_prompts.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from vllm import LLM, SamplingParams
-import ujson as json
+#import ujson as json
 from transformers import AutoTokenizer
 import os
 import argparse
@@ -8,7 +8,8 @@ import torch
 import random
 import numpy as np
 import pandas as pd
-from collections import defaultdict
+#from collections import defaultdict
+from tqdm import tqdm
 
 
 def enforce_reproducibility(seed=1000):
@@ -48,7 +49,7 @@ if __name__ == '__main__':
     input_path = args.input_path #'./wildchat_subset_20.jsonl'
     prompt_path = args.prompt_path
     model_path = args.model_path #'mistralai/Mistral-7B-Instruct-v0.3'
-    output_path = args.output_path #'./wildchat_subset_Mistral-7B-v0.3.txt'
+    output_path = args.output_path #'./wildchat_subset_Mistral-7B-v0.3.jsonl'
 
     cache_dir = None
     if 'HF_MODEL_CACHE' in os.environ:
@@ -65,24 +66,29 @@ if __name__ == '__main__':
     llm = LLM(model=model_path, trust_remote_code=True, max_model_len=8192, download_dir=cache_dir, tensor_parallel_size=args.num_gpus)
     sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=1024)
 
-    batch = [
-        tokenizer.apply_chat_template([
-            {"role": "user", "content": prompt.replace('[TURN1]', row['human_turn_1']).replace('[TURN2]', row['ai_turn_2'])}
-        ], tokenize=False, add_special_tokens=False, add_generation_prompt=True)
-        for index, row in data.iterrows()
-        for j, prompt in enumerate(prompts['Prompt Design'])
-    ]
     print("Generating...")
-    output_batch = llm.generate(batch, sampling_params)
+    for j, prompt in tqdm(enumerate(prompts['Prompt Design']), total=len(prompts)):
+        batch = [
+            tokenizer.apply_chat_template([
+                {"role": "user", "content": prompt.replace('[TURN1]', row['human_turn_1']).replace('[TURN2]', row['ai_turn_2'])}
+            ], tokenize=False, add_special_tokens=False, add_generation_prompt=True)
+            for index, row in data.iterrows()
+        ]
+    
+        output_batch = llm.generate(batch, sampling_params)
+        data[f"Prompt_{j+1}"] = [output.outputs[0].text.strip() for output in output_batch]
 
-    results = defaultdict(list)
-    k = 0
-    for index, row in data.iterrows():
-        for j, prompt in prompts.iterrows():
-            results[prompt['Prompt ID']].append(output_batch[k].outputs[0].text)
-            k += 1
+    data.to_json(output_path, orient='records', lines=True)
+
+
+    # results = defaultdict(list)
+    # k = 0
+    # for index, row in data.iterrows():
+    #     for j, prompt in prompts.iterrows():
+    #         results[prompt['Prompt ID']].append(output_batch[k].outputs[0].text)
+    #         k += 1
 
     #results = {i: output.outputs[0].text for i, output in enumerate(output_batch)}
 
-    with open(output_path, 'wt') as output_file:
-        output_file.write(json.dumps(results))
+    # with open(output_path, 'wt') as output_file:
+    #     output_file.write(json.dumps(results))
