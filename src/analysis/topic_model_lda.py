6600de2aa729ee23ccc5074833097f34876ef4f5
Hua Shen <huashen218@gmail.com>
2024-07-26 16:17:29 -0400

    topic model

--- src/analysis/topic_model_lda.py
+++ src/analysis/topic_model_lda.py
@@ -1,20 +1,23 @@
+import matplotlib.pyplot as plt
 import pdb
+import os
 import argparse
 import pandas as pd
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 from typing import List
 from sklearn.decomposition import NMF, LatentDirichletAllocation
 from sklearn.feature_extraction.text import CountVectorizer
 
 
+
 def plot_top_words(model,
                    feature_names:List[str], 
                    n_top_words:int, 
-                   title:str):
+                   title:str,
+                   output_path:str):
     """Plots the topic clusters with each cluster 20 topics.
     """
-    fig, axes = plt.subplots(2, 5, figsize=(30, 15), sharex=True)
+    fig, axes = plt.subplots(20, 5, figsize=(150, 15), sharex=True)
     axes = axes.flatten()
     for topic_idx, topic in enumerate(model.components_):
         top_features_ind = topic.argsort()[-n_top_words:]
@@ -31,22 +34,27 @@ def plot_top_words(model,
 
     plt.subplots_adjust(top=0.90, bottom=0.05, wspace=0.90, hspace=0.3)
     plt.show()
-    plt.savefig("/shared/0/projects/research-jam-summer-2024/data/topic_data/100k-with-topics.pdf", dpi=150)
+    plt.savefig(os.path.join(output_path, 'topic_features_figure.pdf'), dpi=150)
 
 
 def extract_topics(model,
                    feature_names:List[str], 
-                   n_top_words:int):
+                   n_top_words:int,
+                   output_path:str):
     """Plots the topic clusters with each cluster 20 topics.
     """
     topic_features = []
     topic_weights = []
-    for _, topic in enumerate(model.components_):
-        top_features_ind = topic.argsort()[-n_top_words:]
-        top_features = feature_names[top_features_ind]
-        weights = topic[top_features_ind]
-        topic_features.append(top_features)
-        topic_weights.append(weights)
+    with open(os.path.join(output_path, 'topic_features_weights.txt'), 'a') as the_file:
+    
+        for _, topic in enumerate(model.components_):
+            top_features_ind = topic.argsort()[-n_top_words:]
+            top_features = feature_names[top_features_ind]
+            weights = topic[top_features_ind]
+            topic_features.append(top_features)
+            topic_weights.append(weights)
+            the_file.write(f'{top_features}\n')
+            the_file.write(f'{weights}\n')
     return topic_features, topic_weights
 
 
@@ -62,46 +70,46 @@ def load_tf_features(n_features: int,
     return tf, tf_feature_names
 
 
-def lda_model(n_components: int):
+def lda_model(n_topics: int):
     """Loads the LDA model.
     """
     return LatentDirichletAllocation(
-        n_components=n_components,
-        max_iter=5,
+        n_components=n_topics,
+        max_iter=20,
         learning_method="online",
         learning_offset=50.0,
         random_state=0)
+
+
+def process_string(input:str):
+    return input.lower()
 
 
 def main(args):
 
     dataset = pd.read_json(args.input_path, orient='records', lines=True)
 
-    topic_classes = []
-    topic_texts   = []
-    for _, row in tqdm(dataset.iterrows()):
-        topic_classes.append(row['label'])
-        topic_texts.append(row['text'])
+    training_data = [process_string(t) for t in dataset['human_turn_1'].tolist()]
+    training_data = [d for d in training_data if d.strip()]
 
-    args.n_samples = len(topic_texts)
-    data_samples = topic_texts[:args.n_samples]
+    args.n_samples = len(training_data)
+    data_samples = training_data[:args.n_samples]
 
     tf, tf_feature_names = load_tf_features(args.n_features, data_samples)
-    lda = lda_model(args.n_components)
+    lda = lda_model(args.n_topics)
     lda.fit(tf)
 
-    topic_features, topic_weights = extract_topics(lda, tf_feature_names, args.n_top_words)
-    plot_top_words(lda, tf_feature_names, args.n_top_words, "Topics in LDA model")
-
+    topic_features, topic_weights = extract_topics(lda, tf_feature_names, args.n_top_words, args.output_path)
+    plot_top_words(lda, tf_feature_names, args.n_top_words, "Top 100 Topics in LDA model", args.output_path)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_path", type=str, help="Name of the file with the prompts", default="/shared/0/projects/research-jam-summer-2024/data/topic_data/100k-with-topics.jsonl")
-    parser.add_argument("--output_path", type=str, help="Name of the file to save the generated text")
+    parser.add_argument("--input_path", type=str, help="Name of the file with the prompts", default="/shared/0/projects/research-jam-summer-2024/data/english_only/100k_results/wildchat_subset_en_100k_Mixtral-8x7B.jsonl")
+    parser.add_argument("--output_path", type=str, help="Name of the file to save the generated text", default='/shared/0/projects/research-jam-summer-2024/data/topic_data/lda-outputs-100k_Mixtral-8x7B')
     parser.add_argument("--n_samples", type=int, help="number of samples",default=2000)
     parser.add_argument("--n_features", type=int, help="number of feature",default=1000)
-    parser.add_argument("--n_components", type=int, help="number of compments in LDA",default=10)
+    parser.add_argument("--n_topics", type=int, help="number of compments in LDA",default=100)
     parser.add_argument("--n_top_words", type=int, help="number of top words",default=20)
     args = parser.parse_args()
 
--- src/analysis/topic_model_mallet.py
+++ src/analysis/topic_model_mallet.py
@@ -4,38 +4,28 @@ import pandas as pd
 import pdb
 import argparse
 
-path_to_mallet = "/shared/0/resources/mallet/mallet-2.0.8/bin/mallet"
+
+PATH_TO_MALLET = "/shared/0/resources/mallet/mallet-2.0.8/bin/mallet"
 
 def main(args):
 
     dataset = pd.read_json(args.input_path, orient='records', lines=True)
 
-    
-
-    training_data = [lmw.process_string(t) for t in dataset['text'].tolist()]
+    training_data = [lmw.process_string(t) for t in dataset['human_turn_1'].tolist()]
     training_data = [d for d in training_data if d.strip()]
-    
     lmw.print_dataset_stats(training_data)
 
-    num_topics = 20  # CHANGE THIS TO YOUR PREFERRED NUMBER OF TOPICS
-
-    output_directory_path = '/shared/0/projects/research-jam-summer-2024/data/topic_data/mallet-outputs' # CHANGE THIS TO YOUR OUTPUT DIRECTORY
-
-    topic_keys, topic_distributions = lmw.quick_train_topic_model(path_to_mallet, 
-                                                              output_directory_path, 
-                                                              num_topics, 
+    topic_keys, topic_distributions = lmw.quick_train_topic_model(PATH_TO_MALLET, 
+                                                              args.output_path, 
+                                                              args.n_topics, 
                                                               training_data)
-
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_path", type=str, help="Name of the file with the prompts", default="/shared/0/projects/research-jam-summer-2024/data/topic_data/100k-with-topics.jsonl")
-    parser.add_argument("--output_path", type=str, help="Name of the file to save the generated text")
-    parser.add_argument("--n_samples", type=int, help="number of samples",default=2000)
-    parser.add_argument("--n_features", type=int, help="number of feature",default=1000)
-    parser.add_argument("--n_components", type=int, help="number of compments in LDA",default=10)
-    parser.add_argument("--n_top_words", type=int, help="number of top words",default=20)
+    parser.add_argument("--input_path", type=str, help="Name of the file with the prompts", default="/shared/0/projects/research-jam-summer-2024/data/english_only/100k_results/wildchat_subset_en_100k_Mixtral-8x7B.jsonl")
+    parser.add_argument("--output_path", type=str, help="Name of the file to save the generated text", default='/shared/0/projects/research-jam-summer-2024/data/topic_data/mallet-outputs-100k_Mixtral-8x7B')
+    parser.add_argument("--n_topics", type=int, help="number of compments in LDA",default=100)
     args = parser.parse_args()
 
 
