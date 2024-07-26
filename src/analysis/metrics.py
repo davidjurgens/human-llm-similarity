c64282387471b0ac44c924dd75306efdb53ce292
abrahami <avrahami.isr@gmail.com>
2024-07-26 10:36:33 -0400

    Update metrics.py for LIWC fixing

--- src/analysis/metrics.py
+++ src/analysis/metrics.py
@@ -135,9 +135,9 @@ if __name__ == '__main__':
 
     if 'all' in metrics or 'liwc' in metrics:
         liwc_extractor_obj = LiwcDistExtractor(agg_results=False, normalize=True)
-        human_liwc = liwc_extractor_obj.extract_liwc_occurrences(data['human_turn_3'])[0]
-        llm_liwc = liwc_extractor_obj.extract_liwc_occurrences(data['llm_turn_3'])[0]
-        liwc = liwc_extractor_obj.liwc_similarity(human_liwc, llm_liwc, method="jsd")
+        human_liwc = liwc_extractor_obj.extract_liwc_occurrences(data['human_turn_3'].to_list())
+        llm_liwc = liwc_extractor_obj.extract_liwc_occurrences(data['llm_turn_3'].to_list())
+        liwc = [liwc_extractor_obj.liwc_similarity(human, llm, method="jsd") for human, llm in zip(human_liwc, llm_liwc)]
         data.insert(len(data.columns), "metric_liwc", liwc)
 
     if 'all' in metrics or 'sbert' in metrics:
