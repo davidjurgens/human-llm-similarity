f501355872cd0b21d1bf846e6221e490615d51e0
abrahami <avrahami.isr@gmail.com>
2024-08-01 09:53:44 -0400

    Updating the readability function

--- src/analysis/readability_score.py
+++ src/analysis/readability_score.py
@@ -7,37 +7,21 @@ import multiprocessing as mp
 import textstat
 import glob
 import os
+import numpy as np
 
 debug_mode = False
 n_cpus = 1 if debug_mode else 100
 package_to_use = 'textstat'
 
 
-# OLD CODE - not used
-# def get_readability(idx, text, package='textstat'):
-#     if package == 'textstat':
-#         if len(text.split()) < 10:
-#             return {'flesch_kincaid': -1, 'flesch': -1}
-#         else:
-#             return {'flesch_kincaid': textstat.flesch_kincaid_grade(text), 'flesch': textstat.flesch_reading_ease(text)}
-#     elif package == 'readability':
-#         try:
-#             r = Readability(text)
-#             return {'flesch_kincaid': r.flesch_kincaid().score, 'flesch': r.flesch().score}
-#         # Handle cases with less than 100 words
-#         except ReadabilityException:
-#             return {'flesch_kincaid': -1, 'flesch': -1}
-#     else:
-#         raise IOError("Invalid input package name provided.")
-
-
 def get_flesch_readability(idx, text, normalize=True):
+    # Note! in some cases we return None!
     if len(text.split()) < 10:
-        return {'flesch': -1}
+        return {'flesch': None}
     else:
         readability_score = textstat.flesch_reading_ease(text)
         if readability_score < 0:
-            return {'flesch': -1}
+            return {'flesch': None}
         if normalize:
             normalized_score = readability_score / 110.0
             return {'flesch': normalized_score if normalized_score < 1 else -1}
@@ -64,6 +48,14 @@ def get_dataframe_readability(df, package='textstat', n_cpus=100):
     return df_combined
 
 
+def readability_single_column(data, package='textstat', n_cpus=100):
+    input_for_pool = [(idx, cd, package) for idx, cd in enumerate(data)]
+    pool = mp.Pool(processes=n_cpus)
+    with pool as pool:
+        cur_column_score = pool.starmap(get_flesch_readability, input_for_pool)
+    return np.array(pd.DataFrame(cur_column_score))
+
+
 if __name__ == '__main__':
     data_path = '/shared/0/projects/research-jam-summer-2024/data/english_only/100k_results'
     jsonl_files_found = glob.glob(opj(data_path, '*.jsonl'))
@@ -71,8 +63,14 @@ if __name__ == '__main__':
     f_name_to_save = os.path.basename(file_to_analyze).split('.jsonl')[0] + '_readability_normalized.csv'
     required_columns = ['human_turn_3', 'Prompt_11', 'Prompt_15', 'Prompt_19']
     data = pd.read_json(file_to_analyze, orient='records', lines=True)
+    # there are two ways to get the value.
+    # OPTION A: pass ALL columns required for prediction
     data_subset = data[required_columns].copy()
     data_with_readability_scores = get_dataframe_readability(df=data_subset, package=package_to_use, n_cpus=n_cpus)
-    #data_with_readability_scores.to_csv(opj('/shared/0/projects/research-jam-summer-2024/results', f_name_to_save), index=False)
-    print(data_with_readability_scores.shape)
-    print(data_with_readability_scores.iloc[0])
+    # data_with_readability_scores.to_csv(opj('/shared/0/projects/research-jam-summer-2024/results', f_name_to_save), index=False)
+    # print(data_with_readability_scores.shape)
+    # print(data_with_readability_scores.iloc[0])
+
+    # OPTION B: get prediction for a single column only
+    # readability_scores = readability_single_column(data['Prompt_11'])
+
