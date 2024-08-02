91178e1ac286ac49b6a7233f07235b408955135a
Anders Giovanni Møller <andersgiovanni@gmail.com>
2024-08-02 10:54:17 -0400

    More

--- src/analysis/metrics-russian.py
+++ src/analysis/metrics-russian.py
@@ -10,3 +10,59 @@ from scipy.spatial.distance import jensenshannon
 from numpy import log
 import re
 import lmppl
+
+def enforce_reproducibility(seed=1000):
+    # Sets seed manually for both CPU and CUDA
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    # For atomic operations there is currently
+    # no simple way to enforce determinism, as
+    # the order of parallel operations is not known.
+    # CUDNN
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # System based
+    random.seed(seed)
+    np.random.seed(seed)
+
+
+def load_hf_model(model_name):
+    cache_dir = None
+    if 'HF_MODEL_CACHE' in os.environ:
+        cache_dir = os.environ['HF_MODEL_CACHE']
+    pipe = pipeline("text-classification", model=model_name, model_kwargs={"cache_dir": cache_dir},
+                    device_map='cuda', max_length=512, truncation=True, return_all_scores=True)
+    return pipe
+
+
+def hf_data(data, max_char_len=2000):
+    for row in data:
+        yield row[:max_char_len]
+
+
+def run_hf_model(data, model_name, column_name):
+    pipe = load_hf_model(model_name)
+    # First, human 3rd turn
+    new_data = []
+    for out in tqdm(pipe(hf_data(data), batch_size=4), total=len(data)):
+        new_data.append([s for s in out])
+    return new_data
+
+def sentiment(human, llm):
+    def convert_to_scalar(data):
+        score_map = {
+            'negative': 0,
+            'neutral': 0.5,
+            'positive': 1,
+        }
+        out_data = []
+        for d in data:
+            out_data.append(sum([score_map[s['label']]*s['score'] for s in d]))
+        return out_data
+
+    human_sentiment = convert_to_scalar(run_hf_model(human, "blanchefort/rubert-base-cased-sentiment", 'sentiment'))
+    llm_sentiment = convert_to_scalar(run_hf_model(llm, "blanchefort/rubert-base-cased-sentiment", 'sentiment'))
+
+    return human_sentiment, llm_sentiment, np.array(llm_sentiment) - np.array(human_sentiment)
+
+
